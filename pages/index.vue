fd9d378626090e84ec6ebfcfa885ddb23744c544
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-08-02 17:54:01 +0800

    feat: updated index

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,25 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        VueRegWizard
-      </h1>
-      <h2 class="subtitle">
-        
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <Logo />
+      <Stepper />
+      <Card />
+      <!-- <Index /> -->
     </div>
   </div>
 </template>
-
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Logo from "~/components/Logo.vue";
+import Stepper from "~/components/Stepper.vue";
+import Card from "~/components/Card.vue";
 export default {
-  components: {
-    Logo
-  }
-}
+	components: {
+		Logo,
+		Stepper,
+		Card,
+	},
+};
 </script>
-
 <style>
 .container {
   margin: 0 auto;
@@ -46,18 +28,18 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  background: rgb(151,42,177);
+  background: linear-gradient(0deg, rgba(151,42,177,1) 0%, rgba(28,58,112,1) 80%, rgba(8,24,50,1) 100%);
 }
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
-  color: #35495e;
+  color: #35495E;
   letter-spacing: 1px;
 }
-
 .subtitle {
   font-weight: 300;
   font-size: 42px;
@@ -65,7 +47,6 @@ export default {
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-
 .links {
   padding-top: 15px;
 }
