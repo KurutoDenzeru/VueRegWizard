f1ea8d1afdab5260a49f376569fe4a1e62aaac4a
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-08-05 15:29:58 +0800

    fix: vee-validate not affecting stepper

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,65 +1,63 @@
 export default {
-	// Global page headers: https://go.nuxtjs.dev/config-head
-	// Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+	// Be sure this is set to false, or Quasar will not work
 	ssr: false,
-
-	// Target: https://go.nuxtjs.dev/config-target
-	target: 'static',
-
+	mode: "universal",
+	/*
+	 ** Headers of the page
+	 */
 	head: {
-		title: 'reg-form',
-		htmlAttrs: {
-			lang: 'en',
-		},
+		title: process.env.npm_package_name || "",
 		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' },
-			{ name: 'format-detection', content: 'telephone=no' },
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{
+				hid: "description",
+				name: "description",
+				content: process.env.npm_package_description || "",
+			},
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
 	},
-
-	// Global CSS: https://go.nuxtjs.dev/config-css
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: { color: "#fff" },
+	/*
+	 ** Global CSS
+	 */
 	css: [
-		'quasar/dist/quasar.css',
-		'@quasar/extras/material-icons/material-icons.css',
+		"quasar/src/css/variables.sass", // Import Quasar variables
+		"quasar/dist/quasar.sass",
 	],
-
-	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	/*
+	 ** Plugins to load before mounting the App
+	 */
 	plugins: ['~/plugins/quasar.js', '~/plugins/vee-validate.js'],
-
-	// Auto import components: https://go.nuxtjs.dev/config-components
-	components: true,
-
-	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-	buildModules: [
-		// https://go.nuxtjs.dev/eslint
-		'@nuxtjs/eslint-module',
-	],
-
-	// Modules: https://go.nuxtjs.dev/config-modules
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: [],
+	/*
+	 ** Nuxt.js modules
+	 */
 	modules: [
-		// https://go.nuxtjs.dev/axios
-		'@nuxtjs/axios',
-		'@nuxtjs/style-resources',
+		// Doc: https://axios.nuxtjs.org/usage
+		"@nuxtjs/style-resources",
+		"@nuxtjs/axios",
 	],
-
-	styleResources: {
-		scss: [
-			'quasar/src/css/variables.sass', // Import Quasar variables
-		],
-	},
-
-	// Axios module configuration: https://go.nuxtjs.dev/config-axios
-	axios: {
-		// Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-		baseURL: '/',
-	},
-
-	// Build Configuration: https://go.nuxtjs.dev/config-build
+	/*
+	 ** Axios module configuration
+	 ** See https://axios.nuxtjs.org/options
+	 */
+	axios: {},
+	/*
+	 ** Build configuration
+	 */
 	build: {
-		transpile: ['/^quasar/'],
+		/*
+		 ** You can extend webpack config here
+		 */
+		transpile: [/^quasar/],
 		extend(config, ctx) { },
 	},
-}
+};
